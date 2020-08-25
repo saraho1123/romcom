23768f6b917d3e1d95ecd55d5b1fa4e0187f82be
Sarah Osgood <sarahjs117@gmail.com>
2020-08-25 18:34:40 -0500

    Start functions to display romcom covers

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ var savedCovers = [
 var currentCover;
 
 // Add your event listeners here 👇
-
+window.addEventListener('onload' displayCover)
 
 // Create your event handlers and other functions here 👇
 
@@ -16,4 +16,25 @@ var currentCover;
 // We've provided one function to get you started
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
+}
+
+function displayCover() {
+  getRandomCovers();
+  // see a cover with a randomly selected image and taglin with 2 random discriptors
+  // loop through array of cover images using getRandomIndex(covers) function
+  // repeat that for title and 2 discriptors
+}
+
+function getRandomCovers() {
+  getRandomIndex(covers);
+}
+
+function getRandomTitle() {
+  getRandomIndex(titles);
+}
+
+function getRandomDescriptors() {
+  var descript1 = getRandomIndex(descriptors);
+  var descript2 = getRandomIndex(descriptors);
+
 }
