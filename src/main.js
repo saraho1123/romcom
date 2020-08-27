3a63264e92517b66e6d34559993fc4f3f0d4487a
Sarah Osgood <sarahjs117@gmail.com>
2020-08-27 17:34:04 -0500

    Complete functionality for make your own cover button only

--- src/main.js
+++ src/main.js
@@ -14,12 +14,16 @@ var coverTagLine2 = document.querySelector(".tagline-2");
 var showRandomButton = document.querySelector(".random-cover-button");
 var homeButton = document.querySelector(".home-button");
 var makeNewButton = document.querySelector(".make-new-button");
+var saveCoverButton = document.querySelector(".save-cover-button");
+var formView = document.querySelector(".form-view");
+var homeView = document.querySelector(".home-view");
+
 
 // Add your event listeners here 👇
 window.onLoad = displayCover(currentCover)
 showRandomButton.addEventListener("click", displayRandomButton);
 makeNewButton.addEventListener("click", displayViewForm);
-homeButton.addEventListener("click", displayHomeView);
+// homeButton.addEventListener("click", displayHomeView);
 
 // Create your event handlers and other functions here 👇
 
@@ -51,5 +55,10 @@ function displayRandomButton() {
 }
 
 function displayViewForm() {
-
+  console.log("It's a beautiful day!")
+  formView.classList.remove("hidden");
+  homeView.classList.add("hidden");
+  saveCoverButton.classList.add("hidden");
+  showRandomButton.classList.add("hidden");
+  homeButton.classList.remove("hidden");
 }
