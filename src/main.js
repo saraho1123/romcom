22869fce540deeb7d46bd0adee119b96e0e1d4cb
Amanda Davidson <AmandaADavidson@hotmail.com>
2020-08-27 19:20:28 -0600

    Complete functionality of the view saved covers button

--- src/main.js
+++ src/main.js
@@ -11,12 +11,16 @@ var coverImage = document.querySelector(".cover-image");
 var coverTitle = document.querySelector(".cover-title");
 var coverTagLine1 = document.querySelector(".tagline-1");
 var coverTagLine2 = document.querySelector(".tagline-2");
+
 var showRandomButton = document.querySelector(".random-cover-button");
 var homeButton = document.querySelector(".home-button");
 var makeNewButton = document.querySelector(".make-new-button");
+var viewCoversButton = document.querySelector(".view-saved-button");
 var saveCoverButton = document.querySelector(".save-cover-button");
+
 var formView = document.querySelector(".form-view");
 var homeView = document.querySelector(".home-view");
+var savedCoversView = document.querySelector(".saved-view");
 
 
 // Add your event listeners here 👇
@@ -24,6 +28,7 @@ window.onLoad = displayCover(currentCover)
 showRandomButton.addEventListener("click", displayRandomButton);
 makeNewButton.addEventListener("click", displayViewForm);
 homeButton.addEventListener("click", displayHomeView);
+viewCoversButton.addEventListener("click", displaySavedCovers);
 
 // Create your event handlers and other functions here 👇
 
@@ -69,4 +74,13 @@ function displayHomeView() {
   showRandomButton.classList.remove("hidden");
   saveCoverButton.classList.remove("hidden");
   homeButton.classList.add("hidden");
+}
+
+function displaySavedCovers() {
+  console.log("Is this working")
+  savedCoversView.classList.remove("hidden");
+  homeView.classList.add("hidden");
+  showRandomButton.classList.add("hidden");
+  saveCoverButton.classList.add("hidden");
+  homeButton.classList.remove("hidden");  
 }
